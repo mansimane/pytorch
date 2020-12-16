977295a9fd08a8ad9b74096064afc30943192a5e
Mansi Mane <mansmane@amazon.com>
2020-12-15 21:50:23 -0800

    Removed extra for loop

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -1135,18 +1135,21 @@ class _MultiProcessingDataLoaderIter(_BaseDataLoaderIter):
 
             # Case 2:
             #     Send order :   0 1 2 3 4
-            #     Receive order: 1 3 2 0 4
+            #     Receive order: 0 1 2 3 4
             #     self._rcvd_idx 0 1 2 3 4
 
-
+            task_ids = iter(self._task_info.keys())
             while self._rcvd_idx < self._send_idx:
-                for task_id in self._task_info.keys():
-                    info = self._task_info[task_id]
-                    worker_id = info[0]
-                    if len(info) == 2 or self._workers_status[worker_id]:  # has data or is still active
-                        break
-                    del self._task_info[task_id]
-                    self._rcvd_idx += 1
+
+                task_id = task_ids.__next__()
+                # Note we can't access task info by index because we if data related to that index is already processsed,
+                # corresponding entry does not exist in task_info
+                info = self._task_info[task_id]
+                worker_id = info[0]
+                if len(info) == 2 or self._workers_status[worker_id]:  # has data or is still active
+                    break
+                del self._task_info[task_id]
+                self._rcvd_idx += 1
             else:
                 # no valid `self._rcvd_idx` is found (i.e., didn't break)
                 if not self._persistent_workers:
