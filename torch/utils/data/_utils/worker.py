01977fe43666efc8e3ec8de99479906e4cc006fd
Mansi Mane <mansmane@amazon.com>
2020-12-18 09:29:26 -0800

    Added delay to any worker randomly for 10% of the data

--- torch/utils/data/_utils/worker.py
+++ torch/utils/data/_utils/worker.py
@@ -201,8 +201,9 @@ def _worker_loop(dataset_kind, dataset, index_queue, data_queue, done_event,
             else:
                 try:
                     data = fetcher.fetch(index)
-                    if worker_id == 0:
+                    if random.randint(0,9) < 1:
                         time.sleep(0.5)
+
                 except Exception as e:
                     if isinstance(e, StopIteration) and dataset_kind == _DatasetKind.Iterable:
                         data = _IterableDatasetStopIteration(worker_id)
